81e41b65172741b110b56890ab1b068512fc8e6d
Pushtimarkana <140783719+Pushtimarkana@users.noreply.github.com>
2024-06-07 21:08:22 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter a;");
+	scanf("%d",&a);
+	printf("Enter b;");
+	scanf("%d",&b);
+	printf("Enter c;");
+	scanf("%d",&c);
+	float average=(a+b+c)/3;
+	printf("average:%f",&average);
+	
+}
